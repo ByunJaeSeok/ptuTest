e293de511c6f46abbc7b9493ac55dc853e3fc935
ByunJaeSeok <wotjrl0914@gmail.com>
2016-02-21 15:41:31 +0900

    시간 전체 해제

--- src/components/PeriodSelector.js
+++ src/components/PeriodSelector.js
@@ -30,7 +30,7 @@ class PeriodSelector extends Component {
     super(props);
 
     this.state = {
-      value: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+      value: [],
     };
 
     this.onPeriodSelector = this.onPeriodSelector.bind(this);
